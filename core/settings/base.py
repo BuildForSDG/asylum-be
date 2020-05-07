52f5c47d8c24b7f0a8727d94e3979c35f21a9e67
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-05-07 12:05:43 +0300

    Remove hard-coded secret key

--- core/settings/base.py
+++ core/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7ga8eccpt%-8g5zaun=xsg)b0y=$)x)5niax0ehzq&svmnl&p5'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # Application definition
@@ -83,7 +85,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Default DRF settings 
+# Default DRF settings
 # https://www.django-rest-framework.org/api-guide/permissions/
 
 REST_FRAMEWORK = {
