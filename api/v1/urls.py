34895e64c5d3e8a642b71089d964278ad34ce3d5
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-05-07 10:39:32 +0300

    Scaffold django app

---
+++ api/v1/urls.py
@@ -0,0 +1,5 @@
+""" API URLs """
+
+urlpatterns = [
+
+]
---
+++ core/settings/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+if 'BSDG_ENV' in os.environ:
+	if os.environ['BSDG_ENV'] == 'STAGING':
+		from . staging import *
+	else:
+		from . production import *
+else:
+	from . local import *
+	
