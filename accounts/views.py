28d6cd3134b3812b31a2353b61d1008bbeee38c4
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-05-19 11:09:56 +0300

    Base user viewset

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+"""User views."""
+
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+
+from . serializers import UserSerializer
+
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all().select_related()
--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,6 +1,12 @@
 """API URLs."""
 
 from django.urls import path, include
+from rest_framework import routers
+
+from accounts.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
 
@@ -9,3 +15,5 @@ urlpatterns = [
     path('auth/register/', include('rest_auth.registration.urls')),
 
 ]
+
+urlpatterns += router.urls
