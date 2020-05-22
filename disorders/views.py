803b60c25daef701453cb6b52c9f14681ce67d57
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-05-22 17:20:14 +0300

    Symptom management endpoints

--- disorders/views.py
+++ disorders/views.py
@@ -1,9 +1,45 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from . models import Disorder
-from . serializers import DisorderSerializer
+from . models import Disorder, Symptom
+from . serializers import DisorderSerializer, SymptomSerializer
 
 
 class DisorderViewSet(viewsets.ModelViewSet):
     queryset = Disorder.objects.filter(verified=True).select_related().prefetch_related()
     serializer_class = DisorderSerializer
+
+    @action(detail=True, methods=['get'])
+    def add_symptom(self, request, pk=None):
+        disorder = self.get_object()
+        title = request.GET.get('title', None)
+
+        if title:
+            try:
+                symptom, _ = Symptom.objects.get_or_create(title=title)
+                disorder.symptoms.add(symptom)
+                serializer = SymptomSerializer(symptom)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Exception as e:
+                resp = {'message': str(e)}
+                return Response(resp, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        resp = {'message': 'A symptom title is required'}
+        return Response(resp, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['get'])
+    def remove_symptom(self, request, pk=None):
+        disorder = self.get_object()
+        symptom_id = request.GET.get('symptom_id', None)
+
+        if symptom_id:
+            try:
+                disorder.symptoms.remove(int(symptom_id))
+                return Response({'message': 'OK'}, status=status.HTTP_200_OK)
+            except Exception as e:
+                resp = {'message': str(e)}
+                return Response(resp, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        resp = {'message': 'A valid symptom id is required'}
+        return Response(resp, status=status.HTTP_400_BAD_REQUEST)
