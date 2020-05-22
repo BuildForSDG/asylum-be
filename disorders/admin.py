7a786e4481966499547ca43c134cdd13a42cc93c
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-05-22 15:15:13 +0300

    Admin settings for disorders app

--- disorders/admin.py
+++ disorders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Disorder, Symptom
+
+
+class DisorderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'verified', 'updated']
+    list_display_links = ['title', ]
+
+
+class SymptomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created']
+    list_display_links = ['title', ]
+
+
+admin.site.register(Disorder, DisorderAdmin)
+admin.site.register(Symptom, SymptomAdmin)
