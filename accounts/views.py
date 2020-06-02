88386a16fb19eaf3cb53c8e3616e8d9d47a5710e
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-02 08:00:47 +0300

    Allow searching through users endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 """User views."""
 
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from . filters import UserFilter
 from . serializers import UserSerializer
@@ -13,4 +14,6 @@ User = get_user_model()
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all().select_related()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_class = UserFilter
+    search_fields = ['username', 'email', 'first_name', 'last_name']
