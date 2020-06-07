4b5b67d1bfa26b6afef2076b956b9019f4888010
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-07 16:23:53 +0300

    Update rating instead of duplication

--- ratings/views.py
+++ ratings/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from . models import Review
 from . serializers import ReviewSerializer
@@ -7,3 +8,15 @@ from . serializers import ReviewSerializer
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     queryset = Review.objects.all().select_related()
+
+    def perform_create(self, serializer):
+        target = serializer.validated_data.get('target', None)
+        qs = Review.objects.filter(reviewer=self.request.user, target=target)
+        if qs:
+            review = self.serializer_class(qs.first(), self.request.data)
+            if review.is_valid():
+                review.save()
+            else:
+                return Response(review.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer.save(reviewer=self.request.user)
