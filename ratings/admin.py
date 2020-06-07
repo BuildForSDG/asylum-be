f36885f30cabb70cb3abecb01a5e66a207a5c182
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-07 15:00:00 +0300

    Admin site settings for review model

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'review', 'updated']
+    list_display_links = ['review', ]
+
+
+admin.site.register(Review, ReviewAdmin)
