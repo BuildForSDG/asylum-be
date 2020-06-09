534b653d8c2ff72fc090b90bbb8e218273ab225b
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-09 11:49:56 +0300

    Use customizable CBV for docs endpoint

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -2,14 +2,13 @@
 
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework_swagger.views import get_swagger_view
 
 from accounts.views import UserViewSet
 from disorders.views import DisorderViewSet, SymptomViewSet
 from notifications.views import InvitationViewSet, MessageViewSet
 from ratings.views import ReviewViewSet
+from . views import SwaggerSchemaView
 
-schema_view = get_swagger_view(title='Asylum API Docs')
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
@@ -25,7 +24,7 @@ urlpatterns = [
 
     path('auth/register/', include('rest_auth.registration.urls')),
 
-    path('docs/', schema_view)
+    path('docs/', SwaggerSchemaView.as_view())
 
 ]
 
